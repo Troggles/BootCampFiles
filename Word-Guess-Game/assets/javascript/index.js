c0a75181e795cdc82a9d99a8527a28682166743f
Adam Griepentrog <adamgriepentrog@Adams-iMac.local>
2018-07-25 15:14:46 -0500

    tweaks

--- Word-Guess-Game/assets/javascript/index.js
+++ Word-Guess-Game/assets/javascript/index.js
@@ -4,21 +4,21 @@ window.onload = function () {
           'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
           't', 'u', 'v', 'w', 'x', 'y', 'z'];
     
-    var categories;         // Array of topics
-    var chosenCategory;     // Selected catagory
-    var getHint ;          // Word getHint
-    var word ;              // Selected word
-    var guess ;             // Geuss
-    var guesses = [ ];      // Stored guesses
-    var lives ;             // Lives
-    var counter ;           // Count correct guesses
-    var space;              // Number of spaces in word '-'
+    var categories;         
+    var chosenCategory;     
+    var getHint ;          
+    var word ;              
+    var guess ;             
+    var guesses = [ ];      
+    var lives ;             
+    var counter ;           
+    var space;              
   
     // Get elements
     var showLives = document.getElementById("mylives");
     var showCatagory = document.getElementById("scatagory");
     var getHint = document.getElementById("hint");
-    // var showClue = document.getElementById("clue");
+    
   
   
   
@@ -85,75 +85,7 @@ window.onload = function () {
       }
     }
   
-        // Animate man
-    // var animate = function () {
-    //   var drawMe = lives ;
-    //   drawArray[drawMe]();
-    // }
-  
-    
-     // Hangman
-  //   canvas =  function(){
-  
-  //     myStickman = document.getElementById("stickman");
-  //     context = myStickman.getContext('2d');
-  //     context.beginPath();
-  //     context.strokeStyle = "#fff";
-  //     context.lineWidth = 2;
-  //   };
-    
-  //     head = function(){
-  //       myStickman = document.getElementById("stickman");
-  //       context = myStickman.getContext('2d');
-  //       context.beginPath();
-  //       context.arc(60, 25, 10, 0, Math.PI*2, true);
-  //       context.stroke();
-  //     }
-      
-  //   draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
-      
-  //     context.moveTo($pathFromx, $pathFromy);
-  //     context.lineTo($pathTox, $pathToy);
-  //     context.stroke(); 
-  // }
-  
-  //    frame1 = function() {
-  //      draw (0, 150, 150, 150);
-  //    };
-     
-  //    frame2 = function() {
-  //      draw (10, 0, 10, 600);
-  //    };
-    
-  //    frame3 = function() {
-  //      draw (0, 5, 70, 5);
-  //    };
-    
-  //    frame4 = function() {
-  //      draw (60, 5, 60, 15);
-  //    };
-    
-  //    torso = function() {
-  //      draw (60, 36, 60, 70);
-  //    };
-    
-  //    rightArm = function() {
-  //      draw (60, 46, 100, 50);
-  //    };
-    
-  //    leftArm = function() {
-  //      draw (60, 46, 20, 50);
-  //    };
-    
-  //    rightLeg = function() {
-  //      draw (60, 70, 100, 100);
-  //    };
-    
-  //    leftLeg = function() {
-  //      draw (60, 70, 20, 100);
-  //    };
-    
-  //   drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+        
   
   
     // OnClick Function
@@ -201,33 +133,10 @@ window.onload = function () {
       result();
       comments();
       selectCat();
-      // canvas();
+      
     }
   
     play();
     
-    // Hint
-  
-    //   hint.onclick = function() {
-  
-    //   //   hints = [
-    //   //     ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
-    //   //     ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
-    //   //     ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
-    //   // ];
-  
-    //   var catagoryIndex = categories.indexOf(chosenCategory);
-    //   // var hintIndex = chosenCategory.indexOf(word);
-    //   // showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
-    // };
-  
-     // Reset
-  
-    // document.getElementById('reset').onclick = function() {
-    //   correct.parentNode.removeChild(correct);
-    //   letters.parentNode.removeChild(letters);
-    //   showClue.innerHTML = "";
-    //   // context.clearRect(0, 0, 400, 400);
-    //   play();
-    // }
+   
   }
