8aaefd60a042ebe62f94cba66b60d1c6634faaa0
Adam Griepentrog <adamgriepentrog@Adams-iMac.local>
2018-10-08 15:35:28 -0500

    initial file setup and server.js load

---
+++ Friend-Finder/server.js
@@ -0,0 +1,33 @@
+
+// Dependencies
+
+
+var express = require("express")
+var bodyParser = require("body-parser")
+
+
+//express 
+
+var app = express();
+
+var PORT = process.env.PORT || 8080
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text())
+
+
+// Routes
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+
+
+
+// Listener
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+    
+});
